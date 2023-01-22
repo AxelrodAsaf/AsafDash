c543efa5c9751d7a4a26e2db9ab66d51c4981c6e
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-01-22 11:06:09 +0200

    Home-weather works by current location;

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
+import axios from "axios";
 import '../styles/App.css';
+import { async } from '@firebase/util';
 
 function Home(props) {
+    const [weatherData, setWeatherData] = useState({});
+    const [localName, setLocalName] = useState('');
+    const [localTemp, setLocalTemp] = useState('');
+    const [localHumidity, setLocalHumidity] = useState('');
+    const [localFeelsLike, setLocalFeelsLike] = useState('');
+    const [localWeatherType, setLocalWeatherType] = useState('');
     var loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+    const [temp, setTemp] = useState(true)
+
 
     // Clock widget:
     const [clock, setClock] = useState();
@@ -17,21 +27,41 @@ function Home(props) {
         };
     }, []);
 
+
     // Weather widget:
-        const [locLati, setLocLati] = useState();
-        const [locLong, setLocLong] = useState();
-        // Get the location
-        useEffect(() => {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                // Get lat
-                setLocLati(position.coords.latitude);
-                // Get long
-                setLocLong(position.coords.longitude);
-                });
-        })
+    const [locLati, setLocLati] = useState();
+    const [locLong, setLocLong] = useState();
+    const weatherAPIkey1 = "387655d2c9cf811e47eca2bb05be0434";
+    const weatherAPIkey2 = "6367cfbb689a28190bcd5a74e0ea3b8a";
+    function getWeatherData() {
         // Send to API
-            // const weatherAPIurl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
-            // const weatherAPIkey = "387655d2c9cf811e47eca2bb05be0434"
+        const weatherAPIurl = `https://api.openweathermap.org/data/2.5/weather?lat=${locLati}&lon=${locLong}&appid=${weatherAPIkey2}`
+        axios.get(weatherAPIurl).then((response) => {
+            setWeatherData(response.data);
+            setLocalName(response.data.name);
+            setLocalFeelsLike(Math.round(response.data.main.feels_like - 273.15));
+            setLocalHumidity(response.data.main.humidity);
+            setLocalTemp(Math.round(response.data.main.temp - 273.15));
+            setLocalWeatherType(response.data.weather[0].main);
+        })
+    }
+    useEffect(() => {
+        // Get the location
+        navigator.geolocation.getCurrentPosition(function (position) {
+            // Get lat
+            setLocLati(position.coords.latitude);
+            // Get long
+            setLocLong(position.coords.longitude);
+        });
+    }, []);
+    useEffect(() => {
+        if (locLati) {
+            getWeatherData();
+        }
+    }, [locLati,locLong])
+    useEffect(()=>{
+        setTemp(!temp)
+    },[localWeatherType])
 
 
     return (
@@ -39,16 +69,42 @@ function Home(props) {
             <Navbar />
 
 
-            <div className='home-widgetgrid'>
-                {/* <div themeLight={themeLight.toString()} className='home-widgetgrid'> */}
-                <h1>Hello {loggedInUser ? loggedInUser.firstName : "guest"}!</h1>
-                <p>Current Date And Time : {clock}</p>
-                <p>Latitude: {locLati}</p>
-                <p>Longitude: {locLong}</p>
-                {/* <div className='home-widget homewidget1'></div>
-                <div className='home-widget homewidget2'></div>
-                <div className='home-widget homewidget3'></div>
-                <div className='home-widget homewidget4'></div> */}
+            <div className={`home-widgetgrid`}>
+                <div className="home-left">
+                    <div className="widget">
+                        <h1 className='home-namewidget'>Hey {loggedInUser ? loggedInUser.firstName : "friend"}!</h1>
+                    </div>
+                    <div className="widget home-clock">
+                        <p>Current Date and Time: </p>
+                        <p><strong>{clock}</strong></p>
+                    </div>
+                    <div className="home-weather widget">
+                        <div className="home-weather-left">
+                            {/* <p>Latitude:</p>
+                            <p>Longitude:</p> */}
+                            <p>City Name:</p>
+                            <p>Conditions:</p>
+                            <p>Temp:</p>
+                            <p>Feels Like:</p>
+                            <p>Humidity:</p>
+                        </div>
+                        <div className="home-weather-right">
+                            {/* <p><strong>{locLati ? locLati : <></>}</strong></p>
+                            <p><strong>{locLong? locLong : <></>}</strong></p> */}
+                            <p><strong>{localName? localName : <></>}</strong></p>
+                            <p><strong>{localWeatherType? localWeatherType : <></>}</strong></p>
+                            <p><strong>{localTemp ? localTemp : <></>}°C   /   {Math.round((localTemp * (9 / 5)) + 32)}°F</strong></p>
+                            <p><strong>{localFeelsLike ? localFeelsLike : <></>}°C   /   {Math.round((localFeelsLike * (9 / 5)) + 32)}°F</strong></p>
+                            <p><strong>{localHumidity? localHumidity : <></>}%</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="home-right">
+                    <div className='testwidget'></div>
+                    <div className='testwidget'></div>
+                    <div className='testwidget'></div>
+                    <div className='testwidget'></div>
+                </div>
             </div>
         </div>
     );
