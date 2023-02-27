8c355363f9ce13d7369c311fa0dd4cb04476d93b
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-02-27 21:40:05 +0200

    +news functionality to page;

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import NewsSection from '../components/NewsSection';
+import axios from 'axios';
 // import Login from '../components/Login';
 
 function News(props) {
     const themeLight = props.themeLight;
     const setThemeLight = props.setThemeLight;
-    // var userLoggedIn = JSON.parse(localStorage.getItem('userLoggedIn'));
+    const [displayData, setDisplayData] = useState();
 
+    // When the page renders, check to see if a user is logged in
+    // Define the userLoggedIn variable from local storage
+    var userLoggedIn = localStorage.getItem('userLoggedIn');
+    const userToken = localStorage.getItem('Dashboard-user-token');
+    // If userLoggedIn is null set the userLoggedIn variable to 'default'
+    if (userLoggedIn === null) {
+        userLoggedIn = 'default';
+    }
+
+    useEffect(() => {
+        async function getUserInfo() {
+            try {
+                const response = await axios.get('http://localhost:8000/getInfo/news', {
+                    headers: { Authorization: userToken }
+                });
+                setDisplayData(response.data.topicData);
+                console.log(`The data received from the server: ${response.data.topicData}`);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getUserInfo();
+    }, [userToken]);
 
     return (
         <div className='news-main'>
-            {/* {userLoggedIn? <Navbar themeLight={themeLight} setThemeLight={setThemeLight}/> : <Login/>} */}
             <Navbar themeLight={themeLight} setThemeLight={setThemeLight} />
-
             <div className="news-page-div">
                 <h1>News</h1>
                 <div className="news-widgets">
-                    <div className="news-left">
-                        <NewsSection searchInput={"Seattle"} />
-                        <NewsSection searchInput={"Seahawks"} />
-                        <NewsSection searchInput={"World News"} />
-                    </div>
-                    <div className="news-right">
-                        <NewsSection searchInput={"Israel"} />
-                        <NewsSection searchInput={"Finance"} />
-                        <NewsSection searchInput={"Trending"} />
-                    </div>
+                    {displayData && displayData.length > 0 && displayData.map((item) => (
+                        <NewsSection key={item} searchInput={item} />
+                    ))}
                 </div>
             </div>
         </div>
