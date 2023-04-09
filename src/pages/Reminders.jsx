149f12d11d597f572f6866d315121f4820c71196
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-04-09 17:22:29 +0300

    Reminders genesis;

---
+++ src/pages/Reminders.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+// import Login from '../components/Login';
+
+function Reminders(props) {
+    const themeLight = props.themeLight;
+    const setThemeLight = props.setThemeLight;
+    // var userLoggedIn = JSON.parse(localStorage.getItem('userLoggedIn'));
+    return (
+        <div>
+            {/* {userLoggedIn? <Navbar themeLight={themeLight} setThemeLight={setThemeLight}/> : <Login/>} */}
+            <Navbar themeLight={themeLight} setThemeLight={setThemeLight} />
+            <h1>Reminders</h1>
+
+
+
+
+        </div>
+    );
+}
+
+export default Reminders;
